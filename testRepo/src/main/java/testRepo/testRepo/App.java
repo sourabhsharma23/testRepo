110c3521cae52b25970b04772afdfe07a41b080a
Sourabh Sharma <Sourabh Sharma@LAPTOP-SS72PRID>
2022-06-16 10:50:30 +0530

    Yes

---
+++ testRepo/src/main/java/testRepo/testRepo/App.java
@@ -0,0 +1,13 @@
+package testRepo.testRepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ testRepo/src/test/java/testRepo/testRepo/AppTest.java
@@ -0,0 +1,20 @@
+package testRepo.testRepo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
